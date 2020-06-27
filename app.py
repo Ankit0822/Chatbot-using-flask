1101311ec4711adc61b6933035bf19ffef768fe7
Ankit kumar Singh <63996877+Ankit0822@users.noreply.github.com>
2020-06-28 00:12:32 +0530

    Update app.py

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ from chatterbot import ChatBot
 from chatterbot.trainers import ChatterBotCorpusTrainer
 app = Flask(__name__)
 #create chatbot
-englishBot = ChatBot("Chatterbot", storage_adapter="chatterbot.storage.MongoDatabaseAdapter")
+englishBot = ChatBot("Chatterbot", storage_adapter="chatterbot.storage.SQLStorageAdapter")
 trainer = ChatterBotCorpusTrainer(englishBot)
 trainer.train("chatterbot.corpus.english") #train the chatter bot for english
 #define app routes
